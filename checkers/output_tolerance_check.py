3c43e290861737efba7e76ef65ebf86f064b7fcf
Katherine Osterried <katherine.osterried@env.ethz.ch>
2017-03-24 14:26:36 +0100

    Initial import of testsuite files

---
+++ checkers/output_tolerance_check.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python2
+
+"""
+COSMO TECHNICAL TESTSUITE
+
+This script checks whether the output of two runs is identical by comparing the
+YUCHKDAT files.
+"""
+
+# built-in modules
+import os, sys
+
+# private modules
+sys.path.append('./tools/')
+from ts_utilities import read_environ, dir_path
+from ts_fortran_nl import get_param
+from sys import maxsize
+import comp_yuchkdat
+import ts_thresholds
+
+# information
+__author__     = "Xavier Lapillonne, David Leutwyler, Santiago Moreno"
+__copyright__  = "Copyright 2012-2016, COSMO Consortium"
+__license__    = "GPL"
+__version__    = "1.1"
+__date__       = "Mon Jul 11 15:44:25 CEST 2016"
+__email__      = "cosmo-wg6@cosmo.org"
+__maintainer__ = "xavier.lapillonne@meteoswiss.ch"
+
+# some global definitions
+yufile   = 'YUCHKDAT'    # name of special testsuite output
+yuswitch = 'lcheck'      # namelist switch controlling YUPRDBG output
+lnout    = 'ngribout'    # namelist entry which specifies the nr ouf output lists
+nlfile  = 'INPUT_IO'    # namelist file containing yuswitch
+nlfile2 = 'INPUT_ORG'    # namelist file containing dt
+
+
+
+def check():
+
+    # get name of myself
+    myname = os.path.basename(__file__)
+    header = myname+': '
+
+    # get environment variables
+    env = read_environ()
+    verbose = int(env['VERBOSE'])
+    rundir = dir_path(env['RUNDIR'])
+    refoutdir = dir_path(env['REFOUTDIR'])
+    namelistdir = dir_path(env['NAMELISTDIR'])
+    tolerance = env['TOLERANCE']
+    forcematch = int(env['FORCEMATCH']) == 1
+
+    # defines the 2 file that belongs logically to the checker
+    yufile1 = rundir + yufile
+    yufile2 = refoutdir + yufile
+
+    # extract timestep
+    try:
+        dt = float(get_param(rundir+nlfile2, 'dt'))
+    except:
+        if verbose:
+            print header+'failed to extract dt from '+rundir+nlfile
+        return 20 # FAIL
+
+    # check for output lists
+    try:
+        nout_list = get_param(rundir+nlfile, lnout)
+    except:
+        if verbose:
+            print header+'no output lists in '+rundir+nlfile
+        return 20 # FAIL
+
+    # check if special testsuite output was activated in every output list
+    if get_param(rundir+nlfile, yuswitch, occurrence=nout_list) in ['.FALSE.', '.false.']:
+        if verbose:
+            print yuswitch+' is set to .false. in '+rundir+nlfile+' for this simulation'
+        return 20 # FAIL
+
+    #check if tolerance file exists in namelistdir
+    tolerance_path=namelistdir + tolerance
+    if os.path.exists(tolerance_path):
+        tolerance_file=tolerance_path   #in namelist dir
+        ltol_file=True
+    else:
+        tolerance_file=''
+        ltol_file=False
+
+    # define tolerances for comparing YUCHKDAT files
+    # Use tolerance file if exists
+    if ltol_file:
+        if verbose==2:
+            print header + 'Using tolerance values from file'
+        elif verbose>2:
+            print header + 'Using tolerance values from file '+tolerance_file
+        try:
+            threshold = ts_thresholds.Thresholds(tolerance_file)
+            threshold_dict = threshold.to_dict()
+            if "CHKDAT" in threshold_dict.keys():
+                tol_out  = threshold_dict["CHKDAT"]
+            else:
+                tol_out = threshold_dict["*"]
+            tol_times  = threshold_dict["steps"]
+            minval = threshold_dict["minval"]
+        except:
+            if verbose:
+                print header+'Error while reading '+tolerance_file
+                print header+'Cannot read one of the follwoing parameter: tol_times,tol_out,minval'
+            return 20 # FAIL
+    else:
+        if verbose>1:
+            print header + 'Using default tolerance values'
+
+        tol_times = [1200,2400,4000]      # time limit for tolerance setting in seconds
+        tol_out  = [1.0e-7,1.0e-4,1.0e-0] # tolerance threshold for all other prognostic fields
+        minval   = 1.0e-9                 # values below min val are not considered
+
+    # Override the tolerances in case FORCEMATCH is enabled
+    if forcematch:
+        tol_times = [maxsize]
+        tol_out   = [0.0]
+        minval    = 0.0
+
+    #set tolerance time step index
+
+    try:
+        # check for bit identical results
+        if verbose>1:
+            print header + 'Checking first if results are bit identical'
+        if verbose>2:
+            print header + 'comp_yuprtest()'
+            print header + '  file1 = '+yufile1
+            print header + '  file2 = '+yufile2
+            print header + '  minval = '+str(-1)
+            print header + '  nts = [%s]' % ','.join(map(str, [maxsize]))
+            print header + '  tol_temp = [%s]' % ','.join(map(str, [0.0]))
+            print header + '  tol_out = [%s]' % ','.join(map(str, [0.0]))
+        err_count_identical = comp_yuchkdat.cmp_(yufile1, yufile2, -1, -1, [0.0], [0.0])
+        if verbose>1:
+            if err_count_identical==0:
+                print header + 'Results are bit identical'
+            else:
+                print header + 'Results are not bit identical'
+        # check if results are within tolerance values
+        if verbose>1:
+            print header + 'Checking if results are within tolerance'
+            print header + '  minimal value : %s' %(minval)
+            print header + '  tolerance time : %s' % ','.join(map(str, tol_times))
+            print header + '  thresholds for all output fields: %s' % ','.join(map(str, tol_out))
+        if verbose>2:
+            print header + 'comp_yuprtest()'
+            print header + '  file1 = '+yufile1
+            print header + '  file2 = '+yufile2
+            print header + '  minval = '+str(minval)
+            print header + '  nts = [%s]' % ','.join(map(str, tol_times))
+            print header + '  tol_out = [%s]' % ','.join(map(str, tol_out))
+        error_count = comp_yuchkdat.cmp_(yufile1, yufile2, 0, minval, tol_times, tol_out)
+        if verbose>1:
+            if error_count==0:
+                print header + 'Results are within thresholds'
+            else:
+                print header + 'Results are not within thresholds'
+
+    except Exception as e:
+        if verbose:
+            print e
+        return 20 # FAIL
+
+    if err_count_identical == 0:
+        return 0 # MATCH
+    elif error_count == 0:
+        return 10 # OK
+    else:
+        return 20 # FAIL
+
+if __name__ == "__main__":
+    sys.exit(check())
+
+
+
---
+++ testsuite.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python2
+
+"""
+COSMO TECHNICAL TESTSUITE
+
+This script runs a set of tests defined in testlist.xml and checks the results
+for correctness using a set of checkers which can be defined for each test.
+
+For further help and command line arguments execute this script without arguments.
+"""
+
+# built-in modules
+import os, sys, string, struct
+import optparse as OP
+import xml.etree.ElementTree as XML
+import logging as LG
+
+# private modules
+sys.path.append("./tools") # this is the generic folder for subroutines
+from ts_error import StopError, SkipError
+from ts_utilities import system_command, change_dir
+import ts_logger as LG
+from ts_test import Test
+from default_values import DefaultValues
+
+# information
+__author__     = "Nicolo Lardelli, Xavier Lapillonne, Oliver Fuhrer, Santiago Moreno"
+__copyright__  = "Copyright 2012-2016, COSMO Consortium"
+__license__    = "GPL"
+__version__    = "1.1"
+__date__       = "Mon Jul 11 15:44:25 CEST 2016"
+__email__      = "cosmo-wg6@cosmo.org"
+__maintainer__ = "xavier.lapillonne@meteoswiss.ch"
+
+
+def setup_configuration():
+    """setup an object to carry some global configuration parameters"""
+
+    # construct empty structure
+    class Empty:
+        pass
+    conf = Empty()
+
+    # save base directory where testsuite is executed
+    conf.basedir = os.getcwd()
+
+    # list of all namelist files which will copied
+    conf.l_files = ['INPUT_ORG','INPUT_ASS','INPUT_DIA','INPUT_DYN','INPUT_INI',\
+                    'INPUT_IO','INPUT_PHY','INPUT_SAT','INPUT_POL','INPUT_IDEAL']
+
+    # namelist file which contains nprocx, nprocy and nprocio
+    conf.par_file = 'INPUT_ORG'
+
+    # namelist file which contains the timestep (dt)
+    conf.dt_file = 'INPUT_ORG'
+
+    # name of file to which test result is being written
+    conf.res_file = 'TEST_RES'
+
+    # name of the main file containing output for testing
+    conf.yufile = 'YUPRTEST'
+
+    # dual namelist parameters (relevant to the testsuite)
+    conf.dual_params = []
+    conf.dual_params.append(('nstop','hstop'))
+    conf.dual_params.append(('hincrad','nincrad'))
+
+    return conf
+
+
+def parse_cmdline():
+    """parse command line options"""
+
+    # the parser is initialized with its description and its epilog
+    parser = OP.OptionParser(description=
+                "Description: this script run a series of tests defined in testlist.xml. For "+
+                "each test a set of checks are carried out, see checkers/README for more "+
+                "information",
+              epilog=
+                "Example: ./testsuite.py -n 16 --color -f --exe=cosmo --mpicmd=\'aprun -n\' -v 1")
+    
+    # defines the number of processor, the number of processors is not a test specific option,
+    # it overrides the values present in the namelist INPUT_ORG
+    parser.set_defaults(nprocs=DefaultValues.nprocs)
+    parser.add_option("-n",type="int",dest="nprocs",
+               help=("number of processors (nprocx*nprocy+nprocio) to use [default=%d]" % DefaultValues.nprocs))
+    
+    # defines the number of I/O processors, not a test specific option
+    parser.set_defaults(nprocio=DefaultValues.nprocio)
+    parser.add_option("--nprocio",type="int",dest="nprocio",
+               help="set number of aynchronous IO processor, [default=<from namelist>]")
+ 
+    # defines the behavior of testsuite after fail or crash
+    parser.add_option("-f","--force",action="store_true",dest="force",default=False,
+               help="do not stop upon error")
+    
+    # set the level of verbosity of the standard output
+    parser.set_defaults(v_level=DefaultValues.v_level)
+    parser.add_option("-v",type="int",dest="v_level",help=("verbosity level 0 to 3 [default=%d]" % DefaultValues.v_level))
+
+    # specifies the syntax of the mpi command
+    parser.set_defaults(mpicmd=DefaultValues.mpicmd)
+    parser.add_option("--mpicmd",dest="mpicmd",type="string",
+               help=("MPI run command (e.g. \"mpirun -n\") [default=\"%s\"]" % DefaultValues.mpicmd))
+
+    # defines the executable name, this overides the definition in testlist.xml if any
+    parser.set_defaults(exe=DefaultValues.exe)
+    parser.add_option("--exe",dest="exe",type="string",
+               help="Executable file, [default=<from testlist.xml>]")
+
+    # defines if the output will be colored or not
+    parser.add_option("--color",action="store_true",dest="color",default=False,
+               help="Select colored output [default=False]")
+
+    # overrides the hstop/nstop options in the namelist and execute the cosmo runs with a given number of steps
+    parser.set_defaults(steps=DefaultValues.steps)
+    parser.add_option("--steps",dest="steps",type="int",action="store",
+               help="Run only specified number of timesteps [default=<from namelist>]")
+
+    # defines if a wrapper for job submission has to be written or not
+    parser.add_option("-w","--wrapper",action="store_true",dest="use_wrappers",default=False,
+               help="Use wrapper instead of executable for mpicmd [default=False]")
+
+    # defines the filename for the redirected standard output
+    parser.set_defaults(stdout=DefaultValues.stdout)
+    parser.add_option("-o",dest="stdout",type="string",action="store",
+               help="Redirect standard output to selected file [default=<stdout>]")
+    
+    # defines the behaviour of the redirected standard output, if appended or overwritten
+    parser.add_option("-a","--append",action="store_true",default=False,dest="outappend",
+               help="Appends standard output if redirection selected [default=False]")
+    
+    # only one test is executed
+    parser.add_option("--only",dest="only",type="string",action="store",
+               help="Run only one test define as type,name (e.g. --only=cosmo7,test_1)")
+
+    # update namelist (no run). This is useful to quickly change all namelist at once 
+    parser.add_option("--update-namelist",dest="upnamelist",action="store_true",default=False,
+               help="Use Testsuite to update namelists (no tests executed)")
+
+    # force bit-reproducible results
+    parser.add_option("--force-match",dest="forcematch",action="store_true",default=(DefaultValues.forcematch == 1),
+               help="Force bit-reproducible results")
+
+    parser.add_option("--tune-thresholds",dest="tune_thresholds",action="store_true",default=DefaultValues.tune_thresholds,
+               help="Change thresholds to always at least return OK")
+
+    parser.add_option("--update-thresholds",dest="update_thresholds",action="store_true",default=DefaultValues.update_thresholds,
+               help="Update the thresholds")
+
+    parser.add_option("--tuning-iterations",dest="tuning_iterations",action="store",default=DefaultValues.tuning_iterations,
+               help="Defines how many times the tuning gets executed")
+
+    parser.add_option("--reset-thresholds",dest="reset_thresholds",action="store_true",default=DefaultValues.reset_thresholds,
+               help="Set all thresholds to 0.0 before tuning")
+
+    # update namelist (no run). This is useful to quickly change all namelist at once 
+    parser.add_option("--update-yufiles",dest="upyufiles",action="store_true",default=False,
+               help="Define new references (no tests executed)")
+
+    # specifies the namelist file
+    parser.set_defaults(testlist=DefaultValues.testlist)
+    parser.add_option("-l","--testlist",dest="testlist",type="string",action="store",default=DefaultValues.testlist,
+               help=("Select the testlist file [default=%s]" % DefaultValues.testlist))
+
+    # timeout value for individual tests
+    parser.set_defaults(timeout=DefaultValues.timeout)
+    parser.add_option("-t","--timeout",dest="timeout",type="int",action="store",default=None,
+               help=("Timeout in s for each test [default=%s]" % DefaultValues.timeout))
+
+    # working directory
+    parser.add_option("--workdir",dest="workdir",type="string",action="store",default="./work",
+               help="Working directory [default=./work]")
+
+    # specifies the tolerance file name for the tolerance checker
+    parser.set_defaults(tolerance=DefaultValues.tolerance)
+    parser.add_option("--tolerance",dest="tolerance",type="string",action="store",default=DefaultValues.tolerance,
+               help=("Select the tolerance file name [default=%s]" % DefaultValues.tolerance))
+
+    # parse
+    try:
+        (options,args)=parser.parse_args()
+    except (OP.OptionError,TypeError):
+        sys.exit("problem parsing command line arguments (check ./testsuite.py -h for valid arguments)")
+
+    # convert forcematch options from logical to integer
+    if options.forcematch:
+        options.forcematch = 1
+    else:
+        options.forcematch = 0
+
+    return options
+
+
+def parse_xmlfile(filename, logger):
+
+    try: 
+        xmltree = XML.parse(filename)
+    except Exception as e:
+        logger.error('Error while reading xml file '+filename+':')
+        logger.error(e)
+        sys.exit(1) # this exits without traceback
+        #raise # this exits with full traceback
+
+    return xmltree.getroot()
+
+
+def setup_logger(options):
+
+    # instantiate logger class
+    logger = LG.Logger(options.stdout, options.outappend, options.color)
+
+    # set verbosity level
+    if options.v_level <= 0:
+      logger.setLevel(LG.ERROR)
+    elif options.v_level == 1:
+      logger.setLevel(LG.WARNING)
+    elif options.v_level == 2:
+      logger.setLevel(LG.INFO)
+    elif options.v_level >= 3:
+      logger.setLevel(LG.DEBUG)
+
+    return logger
+    
+
+def main():
+    """read configuration and then execute tests"""
+
+    # definition of structure carrying global configuration
+    conf = setup_configuration()
+    
+    # parse command line arguments
+    options = parse_cmdline()
+        
+    # redirect standard output (if required)
+    logger = setup_logger(options)
+
+    # hello world!
+    logger.important('TESTSUITE '+__version__)
+
+    # parse the .xml file which contains the test definitions
+    logger.info('Parsing XML ('+options.testlist+')')
+    root = parse_xmlfile(options.testlist, logger)
+
+    # generate work directory
+    status = system_command('/bin/mkdir -p '+options.workdir+'/', logger, throw_exception=False)
+    if status:
+      exit(status)
+
+    # loops over all the tests
+    stop = False
+    for child in root.findall("test"):
+
+        # create test object
+        mytest = Test(child, options, conf, logger)
+        
+        if mytest.run_test():
+            # run test
+            try:
+
+                # if upyufiles=True, no model run.
+                if options.upyufiles:
+                    logger.important('Update YU* files mode, no run')
+                    mytest.update_yufiles()
+                #
+                elif options.update_thresholds:
+                    logger.important('Updating the thresholds  on the current runs')
+                    mytest.options.tune_thresholds = True
+                    mytest.log_file = 'exe.log'
+                    mytest.check()
+                # if upnamelist=True, no model run.
+                elif options.upnamelist:
+                    logger.important('Update namelist mode, no run')
+                    mytest.prepare() # prepare test directory and update namelists
+                    mytest.update_namelist() #copy back namelist in typedir
+                else:
+                    if(mytest.options.tune_thresholds):
+                        mytest.options.pert = 0
+                        for i in range(int(mytest.options.tuning_iterations)):
+                            mytest.prepare() # prepare test directory and update namelists
+                            logger.important("Iteration number {0}".format(i+1))
+                            mytest.prerun() # last preparations (dependencies must have finished)
+                            mytest.start()  # start test
+                            mytest.wait()   # wait for completion of test
+                            mytest.check()  # call checkers for this test
+                            mytest.options.reset_thresholds = False
+                            # 1: Perturb only in the first timestep
+                            # 2: Perturb in every iteration
+                            mytest.options.pert = 2
+                    else:
+                        mytest.options.pert = 0
+                        mytest.prepare() # prepare test directory and update namelists
+                        mytest.prerun() # last preparations (dependencies must have finished)
+                        mytest.start()  # start test
+                        mytest.wait()   # wait for completion of test
+                        mytest.check()  # call checkers for this test
+
+            except SkipError as smessage:
+                mytest.result = 15 # SKIP
+                logger.warning(smessage)
+
+            except StopError as emessage:
+                if str(emessage).strip():
+                    logger.error(emessage)
+                    if not options.force:
+                        stop = True
+
+            # write result
+            mytest.write_result()
+
+            # return into the base directory after each test
+            status = change_dir(conf.basedir, logger)
+
+            # exit if required
+            if stop:
+                break
+
+    # end of testsuite std output
+    logger.important('FINISHED')
+
+
+if __name__ == "__main__":
+    main()
+
