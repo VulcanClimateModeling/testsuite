135cc16584bb6ba2009cd8f1ab6d5057395d2866
Xavier Lapillonne <xavier.lapillonne@meteoswiss.ch>
2018-04-13 16:55:19 +0200

    New diag checker and comparison tools (#18)
    * new generic comparison tool to compare ascii files with tables
    * added new checkers for 'YUPRHUMI' and 'YUPRMASS' files

---
+++ checkers/diag_tolerance_check.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2
+
+"""
+COSMO TECHNICAL TESTSUITE
+
+This script checks cosmo diagnostic files within threshold
+Since the test use a simple compare tools which do not depend
+on the time step only the first ncomplines are considered.
+"""
+
+# built-in modules
+import os, sys
+
+# private modules
+sys.path.append(os.path.join(os.path.dirname(__file__), "../tools"))
+from ts_utilities import read_environ, dir_path, str_to_bool
+from ts_fortran_nl import get_param
+from comp_table import cmp_table
+import ts_thresholds
+
+# information
+__author__     = "Xavier Lapillonne"
+__email__      = "cosmo-wg6@cosmo.org"
+__maintainer__ = "xavier.lapillonne@meteoswiss.ch"
+
+# some global definitions
+yufiles    = ['YUPRHUMI','YUPRMASS']         # name of special output files to be compared
+ncomplines = [21,22]                         # max line to consider
+colpattern = ["xxcccccccccc","xxcccccccccc"] # coloumn pattern
+
+def check():
+
+    # init values
+    err_count_identical=0
+    err_count=0
+
+    # get name of myself
+    myname = os.path.basename(__file__)
+    header = myname+': '
+
+    # get environment variables
+    verbose = int(os.environ['TS_VERBOSE'])
+    namelistdir =  dir_path(os.environ['TS_NAMELISTDIR'])
+    rundir = dir_path(os.environ['TS_RUNDIR'])
+    refoutdir = dir_path(os.environ['TS_REFOUTDIR'])
+    tolerance = os.environ['TS_TOLERANCE']
+
+
+    #get tolerance and minval
+    tolerance_file=namelistdir+tolerance
+    if not os.path.exists(tolerance_file):
+        print('Missing tolerance file %s' %(tolerance_file) )
+        return 20 #FAIL
+
+    thresholds = ts_thresholds.Thresholds(tolerance_file)
+    tol = thresholds.get_threshold("CHKDAT", 0)   # tolerence for t
+    minval = thresholds.minval
+
+    
+    for it in range(len(yufiles)):
+        # defines the 2 file that belongs logically to the checker
+        yufile1 = rundir + yufiles[it]
+        yufile2 = refoutdir + yufiles[it]
+
+        try:
+            # check for bit identical results
+            err = cmp_table(yufile1, yufile2, \
+                            colpattern[it],minval,0,0,ncomplines[it])
+            if err !=0 : err_count_identical=err
+            # check for error within tolerance
+            err = cmp_table(yufile1, yufile2, \
+                            colpattern[it],minval,tol,1,ncomplines[it])
+            if err !=0 : err_count=err
+        except Exception as e:
+            if verbose:
+                print e
+            return 20 # FAIL
+
+    if err_count_identical == 0:
+        return 0 # MATCH
+    elif err_count == 0:
+        return 10 # OK
+    else:
+        return 20 # FAIL
+
+if __name__ == "__main__":
+    sys.exit(check())
+
+
+
---
+++ tools/comp_table.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python2
+
+"""
+COSMO TECHNICAL TESTSUITE
+
+General purpose script to compare two files containing tables
+Only lines with given table pattern are considered
+"""
+
+# built-in modules
+import os, sys, string
+
+# information
+__author__     = "Xavier Lapillonne"
+__maintainer__ = "xavier.lapillonne@meteoswiss.ch"
+
+
+def cmp_table(file1,file2,colpattern,minval,threshold,verbose=1,maxcompline=-1):
+
+    # General purpose script to compare two files containing tables
+    # Only lines with given table column pattern. Column to be compared are marked with c
+    # column to discard with x 
+
+    #init
+    ncomp=0
+    nerror=0
+    lerror=False
+    epsilon=1e-16 #used to avoid division by zero in case minval is zero
+    
+    # check file existence
+    if not(os.path.exists(file1)):
+        print('File %s does not exist' %(file1))
+        return -1
+    elif not(os.path.exists(file2)):
+        print('File %s does not exist' %(file2))
+        print 'File '+file2+' does not exist'
+        return -1
+
+    # convert input
+    colpattern=[x=='c' for x in list(colpattern)]
+    threshold=float(threshold)
+    minval=float(minval)
+    
+    # open file
+    data1=open(file1).readlines()
+    data2=open(file2).readlines()
+
+    # get max record
+    nd1=len(data1)
+    nd2=len(data2)
+
+    # check that files are not empty
+    if nd1==0:
+        print('file %s is empty!' %(file1))
+        return -1
+    if nd2==0:
+        print('file %s is empty!' %(file2))
+        return -1
+
+    if nd1!=nd2 and verbose>1:
+        print('Warning: %s and %s have different size, comparing commun set only \n' %(file1,file2))
+
+    ncdata=min(nd1,nd2)
+    if (maxcompline>0):
+        ncdata=min(ncdata,maxcompline)
+
+
+    # Iterates through the lines
+    for il in range(ncdata):
+        l1=data1[il].split()
+        l2=data2[il].split()
+        l1match=matchColPattern(l1,colpattern)
+        l2match=matchColPattern(l2,colpattern)
+        # compare values if both lines are compatible
+        if l1match and l2match:
+            for ic in range(len(colpattern)):
+                if colpattern[ic]:
+                    v1=float(l1[ic])
+                    v2=float(l2[ic])
+                    val_abs_max=max(abs(v1),abs(v2))
+                    if val_abs_max > minval:
+                        ncomp+=1
+                        diff=abs(v1-v2)/(val_abs_max+epsilon)
+                        if diff>threshold:
+                            nerror+=1
+                            # Print error
+                            if verbose>1:
+                                print('Error %2.2e above %2.2e thresold at line %i, col %i' %(diff,threshold,il+1,ic+1))
+                                print('> %s' %(file1))
+                                print(data1[il])
+                                print('< %s' %(file2))
+                                print(data2[il])
+                            #save line for first error
+                            if not lerror:
+                                differ=diff
+                                linerr=il+1
+                                colerr=ic+1
+                                linerr1=data1[il]
+                                linerr2=data2[il]
+                                
+                            lerror=True
+    
+
+
+    if ncomp==0:
+        print 'Warning :no line to compare'
+        nerror=-2
+
+    if lerror and verbose>0:
+        print('Compared values: %i, errors above threshold: %i ; %i %% ' %(ncomp,nerror,nerror*100./ncomp))
+        if verbose==1:
+            print('First error %2.2e above %2.2e thresold at line %i, col %i' %(differ,threshold,linerr,colerr))
+            print('> %s' %(file1))
+            print(linerr1)
+            print('< %s' %(file2))
+            print(linerr2)
+
+    return nerror
+
+#----------------------------------------------------------------------------
+# Local functions
+def matchColPattern(line,colpattern):
+    if len(line)!=len(colpattern):
+        return False
+
+    try:
+        for i in range(len(colpattern)):
+            if colpattern[i]: f=float(line[i])
+    except ValueError:
+        return False
+        
+    return True
+
+#-----------------------------------
+#execute as a script 
+if __name__ == "__main__":
+
+    if len(sys.argv)==6:
+        cmp_table(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4], \
+             sys.argv[5])
+    elif len(sys.argv)==7:
+        cmp_table(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4], \
+             sys.argv[5],sys.argv[6])
+    elif len(sys.argv)==8:
+        cmp_table(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4], \
+             sys.argv[5],sys.argv[6],sys.argv[7])    
+    else:
+        print '''USAGE : ./comp_table file1 file2 colpattern minval threshold [verbose maxcompline]
+        General purpose script to compare two files containing tables         
+        Only lines with given table column pattern. Column to be compared must be numbers are marked with c 
+        column to discard with x 
+        colpattern c for compare or x for ignore, ex: xccx discard first and last column of a 4 column table 
+'''
+
