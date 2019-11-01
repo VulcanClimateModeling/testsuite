e0503a639d41f736c8c6b0f5dded34ecdd779136
Oliver Fuhrer <oli@ginko.ch>
2019-10-31 22:47:49 -0700

    more to YAML for configurations

--- testsuite.py
+++ testsuite.py
@@ -16,6 +16,7 @@ import xml.etree.ElementTree as XML
 import logging as LG
 import ConfigParser
 import ast
+import yaml
 
 # private modules
 sys.path.append(os.path.join(os.path.dirname(__file__), "./tools")) # this is the generic folder for subroutines
@@ -23,7 +24,6 @@ from ts_error import StopError, SkipError
 from ts_utilities import system_command, change_dir
 import ts_logger as LG
 from ts_test import Test
-from default_values import DefaultValues
 
 # information
 __author__     = "Nicolo Lardelli, Xavier Lapillonne, Oliver Fuhrer, Santiago Moreno"
@@ -35,38 +35,27 @@ __email__      = "cosmo-wg6@cosmo.org"
 __maintainer__ = "xavier.lapillonne@meteoswiss.ch"
 
 
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
 def parse_config_file(filename):
-    config = ConfigParser.RawConfigParser()
 
-    # create empty conf object
-    conf = type('configuration', (), {})()
+    with open(filename) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
 
-    # save base directory where testsuite is executed
-    conf.basedir = os.getcwd()
+    conf = objectview(data)
 
-    try:
-        config.read(filename)
-        conf.l_files     = ast.literal_eval(config.get('ts_config','l_files'))
-        conf.nl_ts_switch= config.get('ts_config','nl_ts_switch')
-        conf.config_nl   = config.get('ts_config','config_nl')
-        conf.par_file    = config.get('ts_config','par_file')
-        conf.dt_file     = config.get('ts_config','dt_file')
-        conf.io_file     = config.get('ts_config','io_file')
-        conf.res_file    = config.get('ts_config','res_file')
-        conf.pert_avail  = config.get('ts_config','pert_avail')
-        conf.yufile      = config.get('ts_config','yufile')
-        conf.dual_params = ast.literal_eval(config.get('ts_config','dual_params'))
-
-    except Exception as e:
-        print('Error while reading config file '+filename+':')
-        print(e)
-        raise # this exits with full traceback
-        
     return conf 
 
 
 def parse_cmdline():
     """parse command line options"""
+
+    # read default values
+    with open(os.path.join(os.path.dirname(__file__), 'conf', 'defaults.yaml')) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    defaults = objectview(data)
 
     # the parser is initialized with its description and its epilog
     parser = OP.OptionParser(description=
@@ -78,135 +67,135 @@ def parse_cmdline():
     
     # defines the number of processor, the number of processors is not a test specific option,
     # it overrides the values present in the namelist INPUT_ORG
-    parser.set_defaults(nprocs=DefaultValues.nprocs)
+    parser.set_defaults(nprocs=defaults.nprocs)
     parser.add_option("-n", type="int", dest="nprocs",
-               help=("number of processors (nprocx*nprocy+nprocio) to use [default=%d]" % DefaultValues.nprocs))
+               help=("number of processors (nprocx*nprocy+nprocio) to use [default=%d]" % defaults.nprocs))
     
     # defines the number of I/O processors, not a test specific option
-    parser.set_defaults(nprocio=DefaultValues.nprocio)
+    parser.set_defaults(nprocio=defaults.nprocio)
     parser.add_option("--nprocio", type="int", dest="nprocio",
                help="set number of asynchronous IO processor, [default=<from namelist>]")
  
     # defines the behavior of testsuite after fail or crash
-    parser.set_defaults(force=DefaultValues.force)
+    parser.set_defaults(force=defaults.force)
     parser.add_option("-f","--force", dest="force", action="store_true", 
-               help="do not stop upon error [default=%r]" % DefaultValues.force)
+               help="do not stop upon error [default=%r]" % defaults.force)
     
     # set the level of verbosity of the standard output
-    parser.set_defaults(v_level=DefaultValues.v_level)
+    parser.set_defaults(v_level=defaults.v_level)
     parser.add_option("-v", type="int", dest="v_level", action="store",
-               help=("verbosity level 0 to 3 [default=%d]" % DefaultValues.v_level))
+               help=("verbosity level 0 to 3 [default=%d]" % defaults.v_level))
 
     # specifies the syntax of the mpi command
-    parser.set_defaults(mpicmd=DefaultValues.mpicmd)
+    parser.set_defaults(mpicmd=defaults.mpicmd)
     parser.add_option("--mpicmd", type="string", dest="mpicmd", action="store",
-               help=("MPI run command (e.g. \"mpirun -n\") [default=\"%s\"]" % DefaultValues.mpicmd))
+               help=("MPI run command (e.g. \"mpirun -n\") [default=\"%s\"]" % defaults.mpicmd))
 
     # defines the executable name, this overides the definition in testlist.xml if any
-    parser.set_defaults(exe=DefaultValues.exe)
+    parser.set_defaults(exe=defaults.exe)
     parser.add_option("--exe", type="string", dest="exe", action="store",
                help="Executable file, [default=<from testlist.xml>]")
 
     # defines if the output will be colored or not
-    parser.set_defaults(color=DefaultValues.color)
+    parser.set_defaults(color=defaults.color)
     parser.add_option("--color", dest="color", action="store_true",
-               help="Select colored output [default=%r]" % DefaultValues.color)
+               help="Select colored output [default=%r]" % defaults.color)
 
     # overrides the hstop/nstop options in the namelist and execute the cosmo runs with a given number of steps
-    parser.set_defaults(steps=DefaultValues.steps)
+    parser.set_defaults(steps=defaults.steps)
     parser.add_option("--steps", type="int", dest="steps", action="store",
                help="Run only specified number of timesteps [default=<from namelist>]")
 
     # defines if a wrapper for job submission has to be written or not
-    parser.set_defaults(use_wrappers=DefaultValues.use_wrappers)
+    parser.set_defaults(use_wrappers=defaults.use_wrappers)
     parser.add_option("-w","--wrapper", dest="use_wrappers", action="store_true", 
-               help="Use wrapper instead of executable for mpicmd [default=%r]" % DefaultValues.use_wrappers)
+               help="Use wrapper instead of executable for mpicmd [default=%r]" % defaults.use_wrappers)
 
     # defines the filename for the redirected standard output
-    parser.set_defaults(stdout=DefaultValues.stdout)
+    parser.set_defaults(stdout=defaults.stdout)
     parser.add_option("-o", type="string", dest="stdout", action="store",
                help="Redirect standard output to selected file [default=<stdout>]")
     
     # defines the behaviour of the redirected standard output, if appended or overwritten
-    parser.set_defaults(outappend=DefaultValues.outappend)
+    parser.set_defaults(outappend=defaults.outappend)
     parser.add_option("-a","--append", dest="outappend", action="store_true",
-               help="Appends standard output if redirection selected [default=%r]" % DefaultValues.outappend)
+               help="Appends standard output if redirection selected [default=%r]" % defaults.outappend)
     
     # only one test is executed
-    parser.set_defaults(only=DefaultValues.only)
+    parser.set_defaults(only=defaults.only)
     parser.add_option("--only", type="string", dest="only", action="store",
                help="Run only one test define as type,name (e.g. --only=cosmo7,test_1) [default=None]")
 
     # update namelist (no run). This is useful to quickly change all namelist at once 
-    parser.set_defaults(upnamelist=DefaultValues.upnamelist)
+    parser.set_defaults(upnamelist=defaults.upnamelist)
     parser.add_option("--update-namelist", dest="upnamelist", action="store_true",
-               help="Use Testsuite to update namelists (no tests executed) [default=%r]" % DefaultValues.upnamelist)
+               help="Use Testsuite to update namelists (no tests executed) [default=%r]" % defaults.upnamelist)
 
     # force bit-reproducible results
-    parser.set_defaults(forcematch=DefaultValues.forcematch)
+    parser.set_defaults(forcematch=defaults.forcematch)
     parser.add_option("--force-match", dest="forcematch", action="store_true",
-               help="Force bit-reproducible results [default=%r]" % DefaultValues.forcematch)
+               help="Force bit-reproducible results [default=%r]" % defaults.forcematch)
 
     # only force bit-reproducible results for base tests
-    parser.set_defaults(forcematch_base=DefaultValues.forcematch_base)
+    parser.set_defaults(forcematch_base=defaults.forcematch_base)
     parser.add_option("--force-match-base", dest="forcematch_base", action="store_true",
                help="Force bit-reproducible results only for base tests, " + 
-                    "i.e. those with name matching test in data/ folder [default=%r]" % DefaultValues.forcematch_base)
+                    "i.e. those with name matching test in data/ folder [default=%r]" % defaults.forcematch_base)
 
     # tune thresholds
-    parser.set_defaults(tune_thresholds=DefaultValues.tune_thresholds)
+    parser.set_defaults(tune_thresholds=defaults.tune_thresholds)
     parser.add_option("--tune-thresholds", dest="tune_thresholds", action="store_true",
-               help="Change thresholds to always at least return OK [default=%r]" % DefaultValues.tune_thresholds)
+               help="Change thresholds to always at least return OK [default=%r]" % defaults.tune_thresholds)
 
     # update thresholds
-    parser.set_defaults(update_thresholds=DefaultValues.update_thresholds)
+    parser.set_defaults(update_thresholds=defaults.update_thresholds)
     parser.add_option("--update-thresholds", dest="update_thresholds", action="store_true",
-               help="Update the thresholds [default=%r]" % DefaultValues.update_thresholds)
+               help="Update the thresholds [default=%r]" % defaults.update_thresholds)
 
     # set number of iterations for tuning
-    parser.set_defaults(tuning_iterations=DefaultValues.tuning_iterations)
+    parser.set_defaults(tuning_iterations=defaults.tuning_iterations)
     parser.add_option("--tuning-iterations", dest="tuning_iterations", action="store",
-               help="Defines how many times the tuning gets executed [default=%d]" % DefaultValues.tuning_iterations)
+               help="Defines how many times the tuning gets executed [default=%d]" % defaults.tuning_iterations)
 
     # set thresholds to zero before tuning
-    parser.set_defaults(reset_thresholds=DefaultValues.reset_thresholds)
+    parser.set_defaults(reset_thresholds=defaults.reset_thresholds)
     parser.add_option("--reset-thresholds", dest="reset_thresholds", action="store_true",
-               help="Set all thresholds to 0.0 before tuning [default=%r]" % DefaultValues.reset_thresholds)
+               help="Set all thresholds to 0.0 before tuning [default=%r]" % defaults.reset_thresholds)
 
     # update namelist (no run). This is useful to quickly change all namelist at once
-    parser.set_defaults(upyufiles=DefaultValues.upyufiles)
+    parser.set_defaults(upyufiles=defaults.upyufiles)
     parser.add_option("--update-yufiles", dest="upyufiles", action="store_true",
-               help="Define new references (no tests executed) [default=%r]" % DefaultValues.upyufiles)
+               help="Define new references (no tests executed) [default=%r]" % defaults.upyufiles)
 
     # specifies the namelist file
-    parser.set_defaults(testlist=DefaultValues.testlist)
+    parser.set_defaults(testlist=defaults.testlist)
     parser.add_option("-l","--testlist", type="string", dest="testlist", action="store",
-               help=("Select the testlist file [default=%s]" % DefaultValues.testlist))
+               help=("Select the testlist file [default=%s]" % defaults.testlist))
 
     # timeout value for individual tests
-    parser.set_defaults(timeout=DefaultValues.timeout)
+    parser.set_defaults(timeout=defaults.timeout)
     parser.add_option("-t","--timeout", type="int", dest="timeout", action="store",
-               help=("Timeout in s for each test [default=%s]" % DefaultValues.timeout))
+               help=("Timeout in s for each test [default=%s]" % defaults.timeout))
 
     # working directory
-    parser.set_defaults(workdir=DefaultValues.workdir)
+    parser.set_defaults(workdir=defaults.workdir)
     parser.add_option("--workdir", type="string", dest="workdir", action="store",
-               help="Working directory [default=%s]" % DefaultValues.workdir)
+               help="Working directory [default=%s]" % defaults.workdir)
 
     # specifies the tolerance file name for the tolerance checker
-    parser.set_defaults(tolerance=DefaultValues.tolerance)
+    parser.set_defaults(tolerance=defaults.tolerance)
     parser.add_option("--tolerance", type="string", dest="tolerance", action="store",
-               help=("Select the tolerance file name [default=%s]" % DefaultValues.tolerance))
+               help=("Select the tolerance file name [default=%s]" % defaults.tolerance))
 
     # flag to run the testsuite for icon
-    parser.set_defaults(icon=DefaultValues.icon)
+    parser.set_defaults(icon=defaults.icon)
     parser.add_option("--icon", dest="icon", action="store_true",
-               help=("Run the testsuite for ICON [default=%s]" % DefaultValues.icon))
+               help=("Run the testsuite for ICON [default=%s]" % defaults.icon))
 
     # name of the config file
-    parser.set_defaults(config_file=DefaultValues.config_file)
+    parser.set_defaults(config_file=defaults.config_file)
     parser.add_option("--config-file", type="string", dest="config_file", action="store",
-               help=("Name of the testsuite configuration file [default=%s]" % DefaultValues.config_file))
+               help=("Name of the testsuite configuration file [default=%s]" % defaults.config_file))
 
     # parse
     try:
